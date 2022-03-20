a1896f728a772b454ab3cf7d49ffb96f6d871f90
Aaron Wang <56976471+aaronwangj@users.noreply.github.com>
2022-03-20 13:39:29 -0400

    Add files via upload

---
+++ compile.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+########################################
+############# CSCI 2951-O ##############
+########################################
+
+# Update this file with commands to compile your code
+javac -classpath /local/projects/cplex/CPLEX_Studio_Academic/12.3/x86_64/cplex/lib/cplex.jar ./src/solver/ip/*.java
